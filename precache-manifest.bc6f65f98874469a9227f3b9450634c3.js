577bf5a36e30d0bc8eb933720d43caa9f01ae10b
RafalskyAndrey13 <mycourses13@gmail.com>
2020-03-09 22:22:09 +0200

    Updates

---
+++ precache-manifest.bc6f65f98874469a9227f3b9450634c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4006e8f1e89564121e8e8f88bab4c4",
+    "url": "/BrainItSite/index.html"
+  },
+  {
+    "revision": "b0d57b39d0e2c62eb4a7",
+    "url": "/BrainItSite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c206e21f381d4a2776c",
+    "url": "/BrainItSite/static/js/2.7daa1420.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/BrainItSite/static/js/2.7daa1420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d57b39d0e2c62eb4a7",
+    "url": "/BrainItSite/static/js/main.4dd8001d.chunk.js"
+  },
+  {
+    "revision": "f40be77f4ecd5bb6f540",
+    "url": "/BrainItSite/static/js/runtime-main.3c2774d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BrainItSite/static/media/logo.5d5d9eef.svg"
+  }
+]);
